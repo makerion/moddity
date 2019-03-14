5e65c59cbaf0f43a6f21a4824649a7e82f391b16
Tim Mecklem <timothy@mecklem.com>
2019-03-13 22:32:17 -0400

    Add a fake driver for use when I’m not near the printer

---
+++ lib/moddity/fake_driver.ex
@@ -0,0 +1,129 @@
+defmodule Moddity.FakeDriver do
+  use GenServer
+
+  import Process, only: [{:send_after, 3}]
+
+  @status_template %{
+    "error" => 0,
+    "job" => %{
+      "current_gcode_number" => 0,
+      "current_line_number" => 0,
+      "file" => "unknown",
+      "file_size" => 2345,
+      "id" => "",
+      "progress" => 0,
+      "source" => "usb",
+      "time_elapsed" => 0
+    },
+    "metadata" => %{"type" => "status", "version" => 1},
+    "printer" => %{
+      "accept_version" => 1,
+      "cpu_id" => "543hhj453442345346634544",
+      "firmware" => %{"name" => "RacingMoon", "version" => "0.14.0"},
+      "model_name" => "MOD-t"
+    },
+    "status" => %{
+      "build_plate" => "Unknown",
+      "extruder_target_temperature" => 0.0,
+      "extruder_temperature" => 162.16,
+      "filament" => "OK",
+      "state" => "STATE_IDLE"
+    },
+    "time" => %{"boot" => 516, "idle" => 0}
+  }
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(_) do
+    send_after(self(), {:update_state, :idle_state}, 4000)
+    {:ok, net_failed()}
+  end
+
+  def get_status do
+    GenServer.call(__MODULE__, {:get_status})
+  end
+
+  def send_gcode(_file) do
+    GenServer.call(__MODULE__, {:send_gcode})
+  end
+
+  def handle_call({:get_status}, _from, state) do
+    {:reply, {:ok, state}, state}
+  end
+
+  def handle_call({:send_gcode}, _from, _state) do
+    send_after(self(), {:update_state, :state_job_queued}, 10_000)
+    {:reply, :ok, state_file_rx()}
+  end
+
+  def handle_info({:update_state, :idle_state}, _state) do
+    {:noreply, idle_state()}
+  end
+
+  def handle_info({:update_state, :state_job_queued}, _state) do
+    send_after(self(), {:update_state, :state_building}, 10_000)
+    {:noreply, state_job_queued()}
+  end
+
+  def handle_info({:update_state, :state_building}, _state) do
+    send_after(self(), {:update_state, :state_exec_pause_cmd}, 10_000)
+    {:noreply, state_building()}
+  end
+
+  def handle_info({:update_state, :state_exec_pause_cmd}, _state) do
+    send_after(self(), {:update_state, :idle_state}, 10_000)
+    {:noreply, state_exec_pause_cmd()}
+  end
+
+  # typically one of the known states as the machine starts, now that there's no server to contact over wifi
+  defp net_failed do
+    @status_template
+    |> put_in(["status", "state"], "STATE_NET_FAILED")
+  end
+
+  defp idle_state do
+    @status_template
+  end
+
+  # receiving file, not yet ready to print (don't press the flashing button yet)
+  defp state_file_rx do
+    @status_template
+    |> put_in(["status", "state"], "STATE_FILE_RX")
+    |> put_in(["status", "extruder_temperature"], 105.78)
+  end
+
+  # push that button!
+  defp state_job_queued do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 200.0)
+    |> put_in(["status", "extruder_temperature"], 105.7)
+    |> put_in(["status", "state"], "STATE_JOB_QUEUED")
+    |> put_in(["job", "file_size"], 1540647)
+  end
+
+  defp state_building do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 200.0)
+    |> put_in(["status", "extruder_temperature"], 211.4)
+    |> put_in(["status", "state"], "STATE_BUILDING")
+    |> put_in(["job", "current_gcode_number"], 4841)
+    |> put_in(["job", "current_line_number"], 4841)
+    |> put_in(["job", "file_size"], 1540647)
+    |> put_in(["job", "progress"], 9)
+    |> put_in(["job", "time-elapsed"], 219)
+  end
+
+  # pause button was pressed
+  defp state_exec_pause_cmd do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 210.0)
+    |> put_in(["status", "state"], "STATE_EXEC_PAUSE_CMD")
+    |> put_in(["job", "current_gcode_number"], 5763)
+    |> put_in(["job", "current_line_number"], 5763)
+    |> put_in(["job", "file_size"], 1540647)
+    |> put_in(["job", "progress"], 10)
+    |> put_in(["job", "time-elapsed"], 290)
+  end
+end
