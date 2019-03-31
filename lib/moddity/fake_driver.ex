3ceddbbf8810ae6ebb6fbc0fb0ca0e270f0e83f2
Tim Mecklem <timothy@mecklem.com>
2019-03-31 00:04:14 -0400

    Add state machine of heating extruder and load/unloading for fake driver

--- lib/moddity/fake_driver.ex
+++ lib/moddity/fake_driver.ex
@@ -34,7 +34,7 @@ defmodule Moddity.FakeDriver do
     "time" => %{"boot" => 516, "idle" => 0}
   }
 
-  def start_link(opts) do
+  def start_link(_opts) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
@@ -56,7 +56,7 @@ defmodule Moddity.FakeDriver do
   end
 
   def unload_filament do
-    GenServer.call(__MODULE__, {:unload_filament}, @timeout)
+    GenServer.call(__MODULE__, {:unload_filament})
   end
 
   def handle_call({:get_status}, _from, state) do
@@ -64,6 +64,7 @@ defmodule Moddity.FakeDriver do
   end
 
   def handle_call({:load_filament}, _from, state) do
+    send_after(self(), {:update_state, :state_loadfil_heating}, 1_000)
     {:reply, :ok, state}
   end
 
@@ -73,6 +74,7 @@ defmodule Moddity.FakeDriver do
   end
 
   def handle_call({:unload_filament}, _from, state) do
+    send_after(self(), {:update_state, :state_remfil_heating}, 1_000)
     {:reply, :ok, state}
   end
 
@@ -93,6 +95,26 @@ defmodule Moddity.FakeDriver do
   def handle_info({:update_state, :state_exec_pause_cmd}, _state) do
     send_after(self(), {:update_state, :idle_state}, 10_000)
     {:noreply, state_exec_pause_cmd()}
+  end
+
+  def handle_info({:update_state, :state_loadfil_heating}, _state) do
+    send_after(self(), {:update_state, :state_loadfil_extruding}, 20_000)
+    {:noreply, state_loadfil_heating()}
+  end
+
+  def handle_info({:update_state, :state_loadfil_extruding}, _state) do
+    send_after(self(), {:update_state, :idle_state}, 20_000)
+    {:noreply, state_loadfil_extruding()}
+  end
+
+  def handle_info({:update_state, :state_remfil_heating}, _state) do
+    send_after(self(), {:update_state, :state_remfil_retracting}, 20_000)
+    {:noreply, state_remfil_heating()}
+  end
+
+  def handle_info({:update_state, :state_remfil_retracting}, _state) do
+    send_after(self(), {:update_state, :idle_state}, 20_000)
+    {:noreply, state_remfil_retracting()}
   end
 
   # typically one of the known states as the machine starts, now that there's no server to contact over wifi
@@ -143,5 +165,33 @@ defmodule Moddity.FakeDriver do
     |> put_in(["job", "file_size"], 1540647)
     |> put_in(["job", "progress"], 10)
     |> put_in(["job", "time-elapsed"], 290)
+  end
+
+  # load filament triggered
+  defp state_loadfil_heating do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 210.0)
+    |> put_in(["status", "state"], "STATE_LOADFIL_HEATING")
+  end
+
+  # filament is loading
+  defp state_loadfil_extruding do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 210.0)
+    |> put_in(["status", "state"], "STATE_LOADFIL_EXTRUDING")
+  end
+
+  # unload filament triggered
+  defp state_remfil_heating do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 210.0)
+    |> put_in(["status", "state"], "STATE_REMFIL_HEATING")
+  end
+
+  # filament is retracting
+  defp state_remfil_retracting do
+    @status_template
+    |> put_in(["status", "extruder_target_temperature"], 210.0)
+    |> put_in(["status", "state"], "STATE_REMFIL_RETRACTING")
   end
 end
