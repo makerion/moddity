5798a505741282798b96b8d7882e381481ab581b
Tim Mecklem <timothy@mecklem.com>
2019-05-11 22:13:06 -0400

    Read from status address after write batches

--- lib/moddity/backend/libusb/gcode.ex
+++ lib/moddity/backend/libusb/gcode.ex
@@ -3,6 +3,8 @@ defmodule Moddity.Backend.Libusb.GCode do
   Handles the transfer complexities of sending a gcode file to the printer
   """
 
+  alias Moddity.Backend.Libusb.Status
+
   import Moddity.Backend.Libusb.Util, only: [{:read_command_response_bytes, 2}]
 
   require Logger
@@ -72,18 +74,31 @@ defmodule Moddity.Backend.Libusb.GCode do
   @chunk_size 5120
   @gcode_timeout 1000
 
-  def transfer_file(_handle, <<>>), do: :ok
+  def transfer_file(handle, data, transfers_remaining_in_batch \\ 20)
 
-  def transfer_file(handle, data) when byte_size(data) < @chunk_size do
-    case LibUsb.bulk_send(handle, 0x04, data, @gcode_timeout) do
-      {:ok, 0} -> :ok
+  def transfer_file(handle, data, 0) do
+    case Status.read_raw_status_bytes(handle, 0x83, <<>>, 500) do
+      {:ok, _} -> transfer_file(handle, data, 20)
       error -> error
     end
   end
 
-  def transfer_file(handle, <<data::binary-size(@chunk_size), rest::binary>>) do
+  def transfer_file(_handle, <<>>, _), do: :ok
+
+  def transfer_file(handle, data, _) when byte_size(data) < @chunk_size do
     case LibUsb.bulk_send(handle, 0x04, data, @gcode_timeout) do
-      {:ok, 0} -> transfer_file(handle, rest)
+      {:ok, 0} ->
+        Logger.debug("Sent #{byte_size(data)}. All done!")
+        transfer_file(handle, <<>>, 0)
+      error -> error
+    end
+  end
+
+  def transfer_file(handle, <<data::binary-size(@chunk_size), rest::binary>>, transfers_remaining_in_batch) do
+    case LibUsb.bulk_send(handle, 0x04, data, @gcode_timeout) do
+      {:ok, 0} ->
+        Logger.debug("Sent #{inspect @chunk_size}, #{inspect byte_size(rest)} remaining")
+        transfer_file(handle, rest, transfers_remaining_in_batch - 1)
       error -> error
     end
   end
--- lib/moddity/backend/libusb/status.ex
+++ lib/moddity/backend/libusb/status.ex
@@ -5,34 +5,33 @@ defmodule Moddity.Backend.Libusb.Status do
 
   require Logger
 
-  def transfer_get_status(handle) do
-    with {:ok, _clear_buffer} <- read_raw_status_bytes(handle, 0x83),
-         {:ok, 0} <- LibUsb.bulk_send(handle, 4, "{\"metadata\":{\"version\":1,\"type\":\"status\"}}", 500) do
+  def transfer_get_status(handle, timeout \\ 500) do
+    with {:ok, _clear_buffer} <- read_raw_status_bytes(handle, 0x83, <<>>, timeout),
+         {:ok, 0} <- LibUsb.bulk_send(handle, 4, "{\"metadata\":{\"version\":1,\"type\":\"status\"}}", timeout) do
 
-      read_status(handle)
+      read_status(handle, 0, timeout)
     end
   end
 
-  defp read_status(handle, error_count \\ 0)
-  defp read_status(_handle, 5), do: {:error, :max_retries}
-  defp read_status(handle, error_count) do
-    with {:ok, data} <- read_raw_status_bytes(handle, 0x83),
+  def read_status(_handle, 5, _), do: {:error, :max_retries}
+  def read_status(handle, error_count, timeout) do
+    with {:ok, data} <- read_raw_status_bytes(handle, 0x83, <<>>, timeout),
          trimmed <- String.replace(data, ~r[{.*}{], "{"),
          {:ok, parsed} <- Jason.decode(trimmed) do
       {:ok, parsed}
     else
       {:error, %Jason.DecodeError{} = error} ->
         Logger.error("Error reading status: #{inspect error}")
-      read_status(handle, error_count + 1)
+        read_status(handle, error_count + 1, timeout)
       error ->
         Logger.error("Error reading status: #{inspect error}")
       error
     end
   end
 
-  defp read_raw_status_bytes(handle, address, acc \\ <<>>) do
-    with {:ok, data} <- LibUsb.bulk_receive(handle, address, 64, 500) do
-      read_raw_status_bytes(handle, address, acc <> data)
+  def read_raw_status_bytes(handle, address, acc, timeout) do
+    with {:ok, data} <- LibUsb.bulk_receive(handle, address, 64, timeout) do
+      read_raw_status_bytes(handle, address, acc <> data, timeout)
     else
       {:error, :LIBUSB_ERROR_TIMEOUT} ->
         {:ok, acc}
