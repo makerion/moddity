8c9fb4e680903531a3b9e6894d643d8e9c0d369b
Tim Mecklem <timothy@mecklem.com>
2019-08-10 16:22:28 -0400

    Don’t expect status response from printer after enter dfu

--- lib/moddity/backend/libusb/firmware.ex
+++ lib/moddity/backend/libusb/firmware.ex
@@ -12,23 +12,10 @@ defmodule Moddity.Backend.Libusb.Firmware do
     with {:ok, _clear_buffer} <- read_raw_status_bytes(handle, 0x83, <<>>, timeout),
          {:ok, 0} <- LibUsb.bulk_send(handle, 0x02, @enter_dfu_mode, timeout) do
 
-      read_status(handle, 0, timeout)
-    end
-  end
-
-  def read_status(_handle, 5, _), do: {:error, :max_retries}
-  def read_status(handle, error_count, timeout) do
-    with {:ok, data} <- read_raw_status_bytes(handle, 0x83, <<>>, timeout),
-         trimmed <- String.replace(data, ~r[{.*}{], "{"),
-         {:ok, parsed} <- Jason.decode(trimmed) do
-      {:ok, parsed}
+      :timer.sleep(2000)
+      :ok
     else
-      {:error, %Jason.DecodeError{} = error} ->
-        Logger.error("Error reading status: #{inspect error}")
-        read_status(handle, error_count + 1, timeout)
-      error ->
-        Logger.error("Error reading status: #{inspect error}")
-      error
+      error -> error
     end
   end
 
